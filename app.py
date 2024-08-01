35da1039e1c0302ac780c955dca1c780faf919ed
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-07-31 23:48:28 -0300

    commit 001: first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Pomodoro")
+root.geometry("400x400")
+label = tk.Label(root, text="¡Bienvenido a la aplicación! ")
+label.pack(pady=20)
+
+
+root.mainloop()
