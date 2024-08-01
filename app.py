31ca6c16cdcc6327d1dbdebc223aef7fe64cc5e3
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-08-01 15:57:52 -0300

    commit 004: size and frame counter

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-import tkinter as tk
 from PIL import Image, ImageTk
+
+try:
+    import Tkinter as tk  #Python 2.0
+except ImportError:
+    import tkinter as tk  #Python 3.0
+
 
 root = tk.Tk()
 
-icon = Image.open('./img/logo.png')  
+# Icon
+icon = Image.open('./img/icon.png')  
 icon_tk = ImageTk.PhotoImage(icon)
 
+root.geometry("300x300+500+200")
+root.resizable(False,False)
 root.iconphoto(True, icon_tk)
-
 root.title("Pomodoro")
-root.geometry("400x400")
 
 
+# Content root
 label = tk.Label(root, text="¡Bienvenido a la aplicación! ")
 label.pack(pady=20)
+
+counter = tk.Frame(root)
+counter.configure(width=200, height=200, bg="red")
+counter.pack()
+
 
 
 root.mainloop()
