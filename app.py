0e100352ce0283e36db0c9e41dc14f608c17565f
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-08-01 00:04:55 -0300

    commit 003: new icon

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 root = tk.Tk()
+
+icon = Image.open('./img/logo.png')  
+icon_tk = ImageTk.PhotoImage(icon)
+
+root.iconphoto(True, icon_tk)
+
 root.title("Pomodoro")
 root.geometry("400x400")
+
+
 label = tk.Label(root, text="¡Bienvenido a la aplicación! ")
 label.pack(pady=20)
 
